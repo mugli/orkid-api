7362dc27f56a5f2dadfbc6227e1809266466f405
Mehdi Hasan <mhasan@omicronlab.com>
2019-08-13 15:45:27 +0600

    We don’t need to hide errors

--- src/apollo.js
+++ src/apollo.js
@@ -1,25 +1,12 @@
 const { ApolloServer } = require('apollo-server-express');
-const { GraphQLError } = require('graphql');
-const { v4 } = require('uuid');
 
 const { schema } = require('./graphql-schema');
-
-let formatError;
-if (process.env.NODE_ENV !== 'development') {
-  formatError = error => {
-    const errId = v4();
-
-    console.log(`GraphQL Error ${errId}`, error);
-    return new GraphQLError(`Internal Error ${errId}`);
-  };
-}
 
 const apollo = redis => {
   const apl = new ApolloServer({
     schema,
     mocks: false,
     tracing: true,
-    formatError,
     cors: true,
     context: ({ req }) => ({
       req,
